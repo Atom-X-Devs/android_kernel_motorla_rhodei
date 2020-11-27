7231bb71b8fbe1c684d44aa2e6cd1a6b146727d1
houdz1 <houdz1@lenovo.com>
2020-11-27 11:09:15 +0800

    dsi_display: remove "MMI_STOPSHIP"
    The function is ready for other project.
    SO remove "MMI_STOPSHIP"
    
    Change-Id: I33d83dc84641f6765ffd8c39cc4a01d19bcf62f6
    Signed-off-by: houdz1 <houdz1@mt.com>
    Reviewed-on: https://gerrit.mot.com/1812530
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_display.c
+++ msm/dsi/dsi_display.c
@@ -8745,7 +8745,6 @@ static void dsi_display_panel_id_notification(struct dsi_display *display)
 	}
 }
 
-/* start of MMI_STOPSHIP section */
 struct dsi_enable_status {
 	struct dsi_display *display;
 	int probed;
