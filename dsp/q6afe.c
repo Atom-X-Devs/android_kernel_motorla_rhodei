37449502d9d8fb23d047c91370fcd187b543d34d
zhouwl <zhouwl@lenovo.com>
2021-06-23 19:44:04 +0800

    q6afe: enlarge payload size for AW calibration
    After upgrading AW SKT v0.2.1, speaker calibration
    is failed, need to enlarge this payload size to 8K,
    verifies on v0.2.0, aw882xx calibration works fine.
    - kernel failure log:
    aw_send_afe_cal_apr: Invalid payload size = 4520
    
    Change-Id: I7d5b9b26c52e4bea06ee74169dcfec27bcdba554
    Signed-off-by: zhouwl <zhouwl@mt.com>
    Reviewed-on: https://gerrit.mot.com/1989533
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- dsp/q6afe.c
+++ dsp/q6afe.c
@@ -3931,8 +3931,8 @@ int aw_send_afe_cal_apr(uint32_t rx_port_id, uint32_t tx_port_id,
 	}
 
 	if (aw_cal->map_data.dma_buf == 0) {
-		/*Minimal chunk size is 4K*/
-		aw_cal->map_data.map_size = SZ_4K;
+		/*Minimal chunk size is 8K*/
+		aw_cal->map_data.map_size = SZ_8K;
 		result = msm_audio_ion_alloc(&(aw_cal->map_data.dma_buf),
 				aw_cal->map_data.map_size,
 				&(aw_cal->cal_data.paddr),&len,
@@ -3959,7 +3959,7 @@ int aw_send_afe_cal_apr(uint32_t rx_port_id, uint32_t tx_port_id,
 		goto err;
 	}
 
-	if (cmd_size > (SZ_4K - sizeof(struct param_hdr_v3))) {
+	if (cmd_size > (SZ_8K - sizeof(struct param_hdr_v3))) {
 		pr_err("%s: Invalid payload size = %d\n", __func__, cmd_size);
 		result = -EINVAL;
 		goto err;
