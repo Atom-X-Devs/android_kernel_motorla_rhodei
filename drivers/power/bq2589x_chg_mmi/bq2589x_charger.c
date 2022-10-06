16e55f356e1eb51e3485100b2dc5ff364a50759e
Forenche <prahul2003@gmail.com>
2022-10-07 02:36:44 +0530

    bq2589x_chg_mmi: Fix type specifier error
    ../drivers/power/bq2589x_chg_mmi/bq2589x_charger.c:1487:8: error: type specifier missing, defaults to 'int' [-Werror,-Wimplicit-int]
    static bq2589x_reuqest_dpdm(struct bq2589x *bq, bool enable)
    ~~~~~~ ^
    int
    
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/power/bq2589x_chg_mmi/bq2589x_charger.c
+++ drivers/power/bq2589x_chg_mmi/bq2589x_charger.c
@@ -1521,7 +1521,7 @@ int bq2589x_get_usb_present(struct bq2589x *bq)
 	return ret;
 }
 
-static bq2589x_reuqest_dpdm(struct bq2589x *bq, bool enable)
+static int bq2589x_reuqest_dpdm(struct bq2589x *bq, bool enable)
 {
 	int ret = 0;
 
